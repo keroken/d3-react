a339f2ea88aee310637bfe311bff133c71c687f5
keroken <kenichi.shinagawa@gmail.com>
2023-05-06 13:41:40 +0900

    fix indent format

--- src/D3Chart.js
+++ src/D3Chart.js
@@ -3,35 +3,35 @@ import * as d3 from 'd3';
 const url = 'https://udemy-react-d3.firebaseio.com/tallest_men.json';
 
 export default class D3Chart {
-    constructor(element) {
-        const svg = d3.select(element)
-            .append("svg")
-                .attr("width", 800)
-                .attr("height", 500)
+  constructor(element) {
+    const svg = d3.select(element)
+      .append("svg")
+        .attr("width", 800)
+        .attr("height", 500)
 
-        d3.json(url).then((data) => {
-            if (data === undefined) {return};
+    d3.json(url).then((data) => {
+      if (data === undefined) {return};
 
-            const y = d3.scaleLinear()
-                .domain([0, 272])
-                .range([0, 500])
+      const y = d3.scaleLinear()
+        .domain([0, 272])
+        .range([0, 500])
 
-            const x = d3.scaleBand()
-                .domain(data.map(d => d.name))
-                .range([0, 800])
-                .padding(0.2)
+      const x = d3.scaleBand()
+        .domain(data.map(d => d.name))
+        .range([0, 800])
+        .padding(0.2)
 
-            const rects = svg.selectAll("rect")
-                .data(data)
-        
-            rects.enter()
-                .append("rect")
-                    .attr("x", d => x(d.name))
-                    .attr("y", 0)
-                    .attr("width", x.bandwidth)
-                    .attr("height", d => y(d.height))
-                    .attr("fill", "gray")
+      const rects = svg.selectAll("rect")
+        .data(data)
+      
+      rects.enter()
+        .append("rect")
+          .attr("x", d => x(d.name))
+          .attr("y", 0)
+          .attr("width", x.bandwidth)
+          .attr("height", d => y(d.height))
+          .attr("fill", "gray")
 
-        })
-    }
+    })
+  }
 }
